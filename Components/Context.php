a4133e262ec8e79f65eb554a7f6c0ac0b9db4ea1
Rakheoana <rakheoana@turati.co.za>
2023-12-30 08:49:40 +0200

    context

---
+++ Components/Context.php
@@ -0,0 +1,42 @@
+<?php
+namespace Components;
+
+class Context{
+
+    public  $root = '';
+    public  $host =  '';
+    public  $siteroot = '';
+    public  $dir = '';
+    public $isLoggedIn = false;
+
+
+    public function __construct(){
+      session_start(); 
+        $this->root     = $_SERVER['HTTP_HOST'];
+        $this->host     = 'http://'.$this->root;
+        $this->siteroot = $this->host.'/umdoni/';
+        $this->dir      = $this->root .'/umdoni/';
+        $this->checkAuth();
+        return;
+    }
+
+
+    public function setLoggedIn($isLoggedIn)
+    {
+        return $this->isLoggedIn = $isLoggedIn;
+    }
+
+
+   public function checkAuth()
+    {
+        if(isset($_SESSION['profile']) && !empty($_SESSION['profile']))
+        {
+            $this->setLoggedIn(true);
+        }
+        else{
+            $this->setLoggedIn(false);
+        }
+    }
+
+
+}
