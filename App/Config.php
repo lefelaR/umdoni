4cfe54396ed543a843c1e4ab88d1b338dfe236d7
Rakheoana <rakheoana@turati.co.za>
2023-12-30 19:12:03 +0200

    git ignore

--- App/Config.php
+++ App/Config.php
@@ -25,13 +25,13 @@ class Config
      * Database user
      * @var string
      */
-    const DB_USER = 'admin';
+    const DB_USER = 'root';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'ytmxlz2&';
+    const DB_PASSWORD = '';
 
     /**
      * Show or hide error messages on screen
