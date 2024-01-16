8b0547a45e038d16010519aa817bf0b9ccc824dd
Rakheoana <rakheoana@turati.co.za>
2024-01-16 12:32:21 +0200

    user objects on the ui

--- App/Controllers/Admin/User.php
+++ App/Controllers/Admin/User.php
@@ -8,10 +8,31 @@
 namespace App\Controllers\Admin;
 use \Core\View;
 use App\Models\Profile;
+use App\Models\Users;
 use Aws\S3\S3Client;
 
 class User extends \Core\Controller
 {
+
+
+    
+    private $bucketName;
+    private $awsAccessKeyId;
+    private $clientId;
+    private $userPoolId;
+    private $region;
+    private $awsSecretAccessKey; 
+
+
+    public function __construct()
+    {
+        $this->awsAccessKeyId  = $_ENV['AWS_ACCESS_KEY_ID'];
+        $this->clientId = $_ENV['AWS_COGNITO_CLIENT_ID'];
+        $this->userPoolId = $_ENV['AWS_COGNITO_USER_POOL_ID'];
+        $this->region = $_ENV['AWS_REGION'];
+        $this->awsSecretAccessKey  =  $_ENV['AWS_SECRET_ACCESS_KEY'];
+        $this->bucketName = $_ENV['BUCKET_NAME'];
+    }
 
     protected function before()
     {
@@ -26,6 +47,33 @@ class User extends \Core\Controller
         view::render('admin/user/index.php', $profile, 'dashboard');
     }
   
+
+    public function update()
+    {
+        $data = $_POST;
+        
+        $data['updatedAt'] = date("Y-m-d H:i:s");
+        try {
+            $id =  Agenda::Update($data);
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
+        redirect('dashboard/agendas/index');
+    }
+
+
+    public function updateImage()
+    {
+        $data = $_POST;
+        $data['updatedAt'] = date("Y-m-d H:i:s");
+        try {
+            $id =  Agenda::Update($data);
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
+        redirect('dashboard/agendas/index');
+    }
+
 
     protected function after()
     {
