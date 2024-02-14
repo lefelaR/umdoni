0c2a5c1b49411994721761cf654ee580c776ae09
Rakheoana <rakheoana@turati.co.za>
2024-02-15 01:01:48 +0200

    sidebar active

--- App/Views/dashboard/newsletters/index.php
+++ App/Views/dashboard/newsletters/index.php
@@ -1,41 +1,47 @@
 <?php
+
 global $context;
+
 $data = $context->data;
 
 $crumbs = getCrumbs();
-echo '
+?>
+
 <div class="row">
     <h1>
-        News
+        Publications
     </h1>
     <div class="col-12 col-md-12 order-md-2 order-first">
         <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-            <ol class="breadcrumb">';
-            foreach ($crumbs as $key => $crumb) 
-            {
-                if($key == (count($crumbs)-1))
-                {
-                    $active = 'active';
-           echo ' <li class="breadcrumb-item '.$active.'" aria-current="page">'.$crumb.'</li>  ';
-                }else{   
-                    $active = '';
-              echo '<li class="breadcrumb-item '.$active.'" aria-current="page">'.$crumb.'</li>';
-                }
-            }
-            echo'
+            <ol class="breadcrumb">
+            <?php
+               
+               
+               foreach ($crumbs as $key => $crumb) 
+               {
+                   if($key == (count($crumbs)-1))
+                   {
+                       $active = 'active';
+                    echo ' <li class="breadcrumb-item '.$active.'" aria-current="page">'.$crumb.'</li>  ';
+                   }else{   
+                       $active = '';
+                 echo '<li class="breadcrumb-item '.$active.'" aria-current="page">'.$crumb.'</li>';
+                   }
+               }
+   
+                  ?>
             </ol>
         </nav>
     </div>
 </div>
-';
-?>
+
 <div class="row">
     <div class="col-md">
         <div class="card">
             <div class="card-header">
-                <p class="card-title fw-light">News List</p>
+            <p class="card-title fw-light">Newsletter List</p>
                 <div class="float-start float-lg-end">
-                    <a class="btn btn-sm" href="<?php echo buildurl("dashboard/news/add") ?>" role="button">
+                    <a class="btn btn-sm" href="<?php echo buildurl("dashboard/publications/add")?>" role="button">
                         <i class="bi bi-plus"></i> Add
                     </a>
                     <button class="btn  btn-sm">
@@ -43,46 +49,46 @@ echo '
                     </button>
                 </div>
             </div>
-            <?php
-            echo '
             <div class="card-content">
+            <?php include('Includes/parts/alerts.php') ?>
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table class="table table-lg" id="table1">
+                    <table class="table table-striped" id="table1">
                             <thead>
                                 <tr>
-                                  
+                                    <th>NO#</th>
                                     <th>TITLE</th>
                                     <th>SUMMARY</th>
-                                    <th>BODY</th>
+                                    <th>PUBLISHER</th>
                                     <th>CREATED DATE</th>
                                     <th>ACTIONS</th>
                                 </tr>
                             </thead>
-                            <tbody>';
+                            <tbody>
+                            
+                            <?php
+                                foreach ($data as $key => $newsletter) {
+                                    $key++;
 
-            foreach ($data as $key => $service) {
-                $key++;
-
-                echo '
+                                    echo '
                                 <tr>
-                                 
-                                    <td>' . $service['title'] . '</td>
-                                    <td>' . $service['subtitle'] . '</td>
-                                    <td> ' . $service['body'] . '</td>
-                                    <td>' . $service['createdAt'] . '</td>
+                                    <td class="text-bold-500">'.$key.'</td>
+                                    <td>'.$newsletter['title'].'</td>
+                                    <td>'.$newsletter['subtitle'].'</td>
+                                    <td>'.$newsletter['publisher'].'</td>
+                                    <td>'.$newsletter['createdAt'].'</td>
                                     <td>
-                                        <a class="btn  btn-sm" href="add?id=' .  $service['id'] . '">
-                                        <i class="bi bi-pencil"></i>
+                                        <a class="btn  btn-sm" href="add?id='.$newsletter['id'].'">
+                                            <i class="bi bi-pencil"></i>
                                         </a>
-                                        <a class="btn btn-sm" href="delete?id=' .  $service['id'] . '"onclick="handleDelete()">
-                                         <i class="bi bi-trash"></i>
-                                    </a>
+                                        <a class="btn  btn-sm" href="delete?id='.$newsletter['id'].'"onclick="handleDelete()">
+                                            <i class="bi bi-trash"></i>
+                                        </a>
                                     </td>
                                 </tr>';
-            }
+                                }
+                                ?>
 
-            echo '
                             </tbody>
                         </table>
                     </div>
@@ -91,4 +97,3 @@ echo '
         </div>
     </div>
 </div>
-';
