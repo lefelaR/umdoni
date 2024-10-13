6aea70a2dbba2e6a09147789262d5ad43cefaeb5
rakheoana <rakgew@gmail.com>
2024-10-13 19:55:40 +0200

    updated cron job

--- Components/Cron.job.php
+++ Components/Cron.job.php
@@ -3,13 +3,19 @@
 use App\Models\CronModel;
 
 
+
 $data = CronModel::getData();
+
 $updated = [];
 
-foreach ($data as $tender) {
-    if ($tender['dueDate'] < date("Y-m-d") ) {
-        $data = CronModel::UdateTime($tender['id']);
-        $updated[$tender['id']] = $data;
+try {
+    foreach ($data as $tender) {
+        if ($tender['dueDate'] < date("Y-m-d") ) {
+            $data = CronModel::UdateTime($tender['id']);
+            $updated[$tender['id']] = $data;
+        }
     }
+    return true;
+} catch (\Throwable $th) {
+    throw $th;
 }
-return true;
--- Components/TimeMachine.php
+++ Components/TimeMachine.php
@@ -1,24 +1,33 @@
 <?php
 
+
+
 namespace Components;
+
+use  App\Models\User;
+use App\Models\Role;
+use App\Models\Permission;
+use App\Models\TendersModel;
+use App\Models\QuotationsModel;
+use App\Models\VacanciesModel;
 
 
 class TimeMachine 
-{
-    
-    public static function goBackInTime(string $time, string $timezone = null): string
     {
-        $time = \DateTime::createFromFormat("", $time);
-        return $time;
-    }
+        
+        public static function goBackInTime(string $time, string $timezone = null): string
+        {
+            $time = \DateTime::createFromFormat("", $time);
+            return $time;
+        }
 
-    public static function goBackToTheFuture(string $time,, string $timezone = null): string
-    {
-        return "";
-    }
+        public static function goBackToTheFuture(string $time,, string $timezone = null): string
+        {
+            return "";
+        }
 
-    public static function matchTime(string $time,  string $timezone = null): string
-    {
-        return "";
+        public static function matchTime(string $time,  string $timezone = null): string
+        {
+            return "";
+        }
     }
-}
