7e321805b6849a1ab13cd2361058a4ab0ef0c992
rakheoana <rakgew@gmail.com>
2024-12-25 11:18:59 +0200

    boom

--- App/Config.php
+++ App/Config.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App;
-
 /**
  * Application configuration
  *
@@ -9,27 +7,38 @@ namespace App;
  */
 class Config
 {
-
     /**
      * Database host
      * @var string
      */
-
     const DB_HOST = 'reseller142.aserv.co.za';
     /**
      * Database namegit 
      * @var string
      */
+    const DB_NAME = 'umdonigov_umdoni';
+
+    /**
+     * Database user
+     * @var string
+     */
     const DB_USER = 'umdonigov_admin';
 
     /**
-   
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = '29019WtP98zj23';
+
+    /**
      * Show or hide error messages on screen
      * @var boolean
+     */
+    const SHOW_ERRORS = false;
+
+
+    /**
      * version
      */
-
-    const SHOW_ERRORS = true;
-
     const VERSION = '1';
 }
